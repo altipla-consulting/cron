be391b5cb0d8d70cab8270e41a260047d8318b87
Ernesto Alejo <ernestokarim@gmail.com>
2018-03-16 19:29:41 +0100

    Usa una librería externa para parsear los schedules de las funciones.

--- cron.go
+++ cron.go
@@ -5,30 +5,30 @@ import (
 	"runtime"
 	"time"
 
+	"github.com/gorhill/cronexpr"
 	"github.com/juju/errors"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 )
 
 type Fn func(ctx context.Context) error
-type Nexter func(t time.Time) time.Time
 
 func Daily(fn Fn) {
-	run(fn, "daily", func(t time.Time) time.Time {
-		return time.Date(t.Year(), t.Month(), t.Day()+1, 1, 0, 0, 0, time.UTC)
-	})
+	Schedule("0 0 1 * * * *", fn)
 }
 
-func run(fn Fn, schedule string, next Nexter) {
+func Schedule(schedule string, fn Fn) {
 	logger := log.WithFields(log.Fields{
 		"name":     runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name(),
 		"schedule": schedule,
 	})
 
+	expr := cronexpr.MustParse(schedule)
+
 	go func() {
-		n := time.Now()
-		logger.WithFields(log.Fields{"next-run": next(n)}).Info("Schedule cron")
-		time.Sleep(next(n).Sub(n))
+		next := expr.Next(time.Now())
+		logger.WithFields(log.Fields{"next-run": next}).Info("Schedule cron")
+		time.Sleep(next.Sub(time.Now()))
 
 		if err := fn(context.Background()); err != nil {
 			logger.WithFields(log.Fields{"err": err.Error(), "stack": errors.ErrorStack(err)}).Error("Failed to run cron")
