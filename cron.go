a1ef5000904228b334f2af53ec97cf87ebee1fac
dvormagic <david.altipla@gmail.com>
2018-04-13 12:21:14 +0200

    Initialize map to alow assign new values in funcs.

--- cron.go
+++ cron.go
@@ -25,7 +25,9 @@ type Runner struct {
 type Option func(runner *Runner)
 
 func NewRunner(opts ...Option) *Runner {
-	runner := new(Runner)
+	runner := &Runner{
+		funcs: map[string]Fn{},
+	}
 	for _, opt := range opts {
 		opt(runner)
 	}
